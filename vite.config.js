edaeae89aed7153ee368eb522875e2301f7fd331
anhnv <anhnv@bfast.dev>
2025-01-09 17:53:08 +0700

    Update code

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/your-repo-name/',
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    port: 3000,
+  },
+});
